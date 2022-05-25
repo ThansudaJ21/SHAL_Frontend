23b3de10db53dbf6a6aa9a8f59253c69eafbc892
Patteera <patteera_mu@cmu.ac.th>
2022-05-25 20:30:32 +0700

    chore: setup the project
    Install TailwindCSS, VueSweetalert2, vue-select

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Store from './store'
+import '@/services/AxiosInterceptorSetup.js'
 
-createApp(App).use(store).use(router).mount("#app");
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+import './styles/custom-sweet-alert.css'
+import './index.css'
+
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
+createApp(App)
+    .use(Store)
+    .use(VueSweetalert2)
+    .use(router)
+    .component('v-select', vSelect)
+    .mount('#app')
