fc576dd786b4b7271167a2bb40440e0e60098806
Patteera <patteera_mu@cmu.ac.th>
2022-05-27 09:36:41 +0700

    faet: add icons

--- src/views/showcase/showcase.vue
+++ src/views/showcase/showcase.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="mx-auto flex max-w-md flex-col gap-4">
-      <h1 class="mt-10 mb-8 text-2xl font-bold text-primary-900 uppercase">
+    <div class="m-auto flex max-w-md flex-col gap-4">
+      <h1 class="mt-10 mb-8 text-2xl font-semibold text-primary-900 uppercase">
         Show Case
       </h1>
 
@@ -16,6 +16,14 @@
       </div>
 
       <p>Button with icon</p>
+      <div class="flex flex-wrap gap-4">
+        <PrimaryButton><AddWhiteIcon />Primary</PrimaryButton>
+        <SecondaryButton><UserOutlinedBlueIcon />Secondary</SecondaryButton>
+        <OutlinedButton variant="default"><PencilIcon />Outlined</OutlinedButton>
+        <DashedButton variant="default"><AddBlueIcon />Dashed</DashedButton>
+        <OutlinedButton variant="error"><TrashOutlinedIcon />Outlined</OutlinedButton>
+        <DashedButton variant="error"><TrashSolidIcon />Dashed</DashedButton>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +33,12 @@ import PrimaryButton from "@/components/button/primary-button.vue";
 import SecondaryButton from "@/components/button/secondary-button.vue";
 import OutlinedButton from "@/components/button/outlined-button.vue";
 import DashedButton from "@/components/button/dashed-button.vue";
+import AddWhiteIcon from "@/assets/icons/add-white.svg?inline";
+import AddBlueIcon from "@/assets/icons/add-blue.svg?inline";
+import UserOutlinedBlueIcon from "@/assets/icons/user-outlined-blue.svg?inline";
+import PencilIcon from "@/assets/icons/pencil-alt.svg?inline";
+import TrashOutlinedIcon from "@/assets/icons/trash-outlined.svg?inline";
+import TrashSolidIcon from "@/assets/icons/trash-solid.svg?inline";
 
 export default {
   name: "ShowCase",
@@ -33,6 +47,12 @@ export default {
     SecondaryButton,
     OutlinedButton,
     DashedButton,
+    AddWhiteIcon,
+    AddBlueIcon,
+    UserOutlinedBlueIcon,
+    PencilIcon,
+    TrashOutlinedIcon,
+    TrashSolidIcon
   },
 };
 </script>
