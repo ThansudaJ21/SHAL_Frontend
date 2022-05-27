3000051bf82a1c16aef137f1eb784d7a168371fa
Patteera <patteera_mu@cmu.ac.th>
2022-05-27 07:43:06 +0700

    feat: implement button component
    Create Base button for primary, secondary, outlined, and dashed button

---
+++ src/components/button/primary-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <BaseButton
+    class="
+      bg-primary-900
+      text-white
+      hover:bg-primary-700
+      active:bg-primary-500
+      disabled:bg-neutral-100 disabled:text-neutral-500
+    "
+    :type="type"
+  >
+    <template v-slot:icon />
+    <slot />
+  </BaseButton>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import BaseButton from "./base-button.vue";
+
+export default defineComponent({
+  name: "PrimaryButton",
+  components: { BaseButton },
+  props: {
+    type: {
+      type: String,
+      default: "button",
+      validator: (prop) => ["button", "submit", "reset"].includes(prop),
+      required: false,
+    },
+    click: {
+      type: Function,
+      required: false,
+    },
+  },
+});
+</script>
---
+++ src/views/showcase/showcase.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="mx-auto flex max-w-md flex-col gap-4">
+      <h1 class="mt-10 mb-8 text-2xl font-bold text-primary-900 uppercase">
+        Show Case
+      </h1>
+
+      <p>Button</p>
+      <div class="flex flex-wrap gap-4">
+        <PrimaryButton>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <OutlinedButton variant="default">Outlined</OutlinedButton>
+        <DashedButton variant="default">Dashed</DashedButton>
+        <OutlinedButton variant="error">Outlined</OutlinedButton>
+        <DashedButton variant="error">Dashed</DashedButton>
+      </div>
+
+      <p>Button with icon</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PrimaryButton from "@/components/button/primary-button.vue";
+import SecondaryButton from "@/components/button/secondary-button.vue";
+import OutlinedButton from "@/components/button/outlined-button.vue";
+import DashedButton from "@/components/button/dashed-button.vue";
+
+export default {
+  name: "ShowCase",
+  components: {
+    PrimaryButton,
+    SecondaryButton,
+    OutlinedButton,
+    DashedButton,
+  },
+};
+</script>
