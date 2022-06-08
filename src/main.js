ff4bb7bff312c37af0d5d113de34d814bd6954a9
Patteera <patteera_mu@cmu.ac.th>
2022-06-08 14:05:43 +0700

    feat: add card component
    add card component

--- src/main.js
+++ src/main.js
@@ -8,13 +8,14 @@ import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 import './styles/custom-sweet-alert.css'
 import './index.css'
-
+import InputFacade from 'vue-input-facade'
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css'
 
 createApp(App)
     .use(Store)
     .use(VueSweetalert2)
+    .use(InputFacade)
     .use(router)
     .component('v-select', vSelect)
     .mount('#app')
