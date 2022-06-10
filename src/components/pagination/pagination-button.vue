88f611a7b6255bcfc1e684f4a30ad3b5f727c0d1
Patteera <patteera_mu@cmu.ac.th>
2022-06-10 18:39:38 +0700

    feat: implement pagination

---
+++ src/components/pagination/pagination-button.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button
+      class="
+        flex
+        h-6
+        w-6
+        items-center
+        justify-center
+        rounded-full
+        text-xs
+        leading-[17px]
+      "
+      :class="[
+        fillBackground && 'bg-primary-200 text-white',
+        currentPage === page && '!bg-primary-500',
+      ]"
+      @click="click"
+    >
+      {{ page }}
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "PaginationButton",
+  props: {
+    click: {
+      type: Function,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    page: {
+      type: Number,
+      required: true,
+    },
+    fillBackground: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+});
+</script>
