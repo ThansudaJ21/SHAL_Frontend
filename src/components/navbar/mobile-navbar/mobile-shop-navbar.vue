a37e3ccf3c5e4e39a92cac09748f5eba11e06d56
Patteera <patteera_mu@cmu.ac.th>
2022-09-06 11:32:27 +0700

    feat: implement my cart page and navbar for shop page

---
+++ src/components/navbar/mobile-navbar/mobile-shop-navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="bg-primary-900 rounded-b-[28px]">
+    <div class="md:hidden h-fit justify-between p-4 items-center space-y-4">
+      <div class="w-full flex justify-between">
+        <div class="flex items-center">
+          <a @click="$router.go(-1)">
+            <BackIcon />
+          </a>
+        </div>
+        <div class="flex items-center justify-center w-full">
+          <div class="rounded-full mr-2">
+            <img class="w-12 h-12 rounded-full" :src="image" alt="shop" />
+          </div>
+          <p class="text-base font-semibold text-white">
+            {{ title }}
+          </p>
+        </div>
+      </div>
+      <div
+        class="w-full flex items-center"
+        v-if="$route.name == 'HomePage' || $route.name == 'SearchResultPage'"
+      >
+        <Form @submit="handleProductSearch" class="w-full">
+          <IconTextField
+            type="text"
+            name="productName"
+            placeholder="What are you looking for?"
+            :showError="false"
+        /></Form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { Form } from "vee-validate";
+import IconTextField from "@/components/field/icon-text-field/icon-text-field.vue";
+import BackIcon from "@/assets/icons/chevron-left.svg?inline";
+
+export default {
+  name: "ShopNavbar",
+  components: {
+    IconTextField,
+    BackIcon,
+    Form,
+  },
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: "SHAL",
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      page: ["ShopPage"],
+    };
+  },
+};
+</script>
---
+++ src/views/buyer/my-cart/my-cart-page.vue
@@ -0,0 +1,95 @@
+<template>
+  <MobileLayout title="My Cart" :image="this.$store.getters.getUser.pictureUrl">
+    <MyCartCard
+      v-for="cart in cartObject"
+      :key="cart.shopId"
+      :shopId="cart.shopId"
+      :productProps="cart.productInCart"
+    />
+  </MobileLayout>
+  <div
+    class="
+      md:hidden
+      w-full
+      h-12
+      px-4
+      flex
+      items-center
+      fixed
+      inset-x-0
+      bottom-0
+      bg-primary-900
+    "
+  >
+    <div class="flex justify-center gap-x-16 w-full">
+      <div class="justify-start w-full">
+        <p class="text-xs leading-[14px] text-white">Total Payment</p>
+        <p class="text-sm leading-[17px] text-white text-semibold">
+          ฿{{ Number(this.$store.getters.getTotalPrice).toLocaleString() }}
+        </p>
+      </div>
+      <div class="flex justify-end">
+        <PrimaryButton class="bg-warning-500 w-24 h-8" :click="buyNow"
+          >Buy Now</PrimaryButton
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { showAlert } from "@/hooks/sweet-alert/sweet-alert.js";
+import TradingService from "@/services/trading/trading-service";
+import MobileLayout from "@/components/layout/mobile-app-layout.vue";
+import PrimaryButton from "@/components/button/primary-button.vue";
+import MyCartCard from "@/components/card/my-cart-card.vue";
+
+export default {
+  name: "MyCartPage",
+  components: {
+    MobileLayout,
+    PrimaryButton,
+    MyCartCard,
+  },
+  data() {
+    return {
+      cartObject: [],
+    };
+  },
+  created() {
+    let shops = [];
+    let products = [];
+    TradingService.getAddToCartProduct(this.$store.getters.getUser.id).then(
+      (response) => {
+        let data = response.data.data.getAddToCartProduct;
+        for (let index = 0; index < data.length; index++) {
+          let shopId = data[index].shop.id;
+          products.push(shopId);
+          if (!shops.includes(shopId)) {
+            shops.push(shopId);
+          }
+        }
+        console.log(shops);
+        for (let index = 0; index < shops.length; index++) {
+          let inCart = [];
+          for (let index2 = 0; index2 < data.length; index2++) {
+            if (shops[index] == products[index2]) {
+              inCart.push(data[index2]);
+            }
+          }
+          this.cartObject.push({
+            shopId: shops[index],
+            productInCart: inCart,
+          });
+        }
+        console.log(this.cartObject);
+      }
+    );
+  },
+  methods: {
+    buyNow() {
+      showAlert("error", "Maintenance", "");
+    },
+  },
+};
+</script>
